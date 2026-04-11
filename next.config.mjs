bf62b7407007c9cb7cebf3c7ed853687ba400c5f
erentaysi <erentaysi@users.noreply.github.com>
2026-04-12 00:55:31 +0300

    feat(scrapers): added smart metadata population (specs, desc) and fixed image whitelists for production

--- next.config.mjs
+++ next.config.mjs
@@ -10,23 +10,27 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '**.dsmcdn.com', // Trendyol dynamic CDNs
+        hostname: '**.dsmcdn.com',
       },
       {
         protocol: 'https',
-        hostname: '**.hepsiburada.net', // Hepsiburada dynamic CDNs
+        hostname: '**.hepsiburada.net',
       },
       {
         protocol: 'https',
-        hostname: '**.media-amazon.com', // Amazon dynamic CDNs
+        hostname: '**.media-amazon.com',
       },
       {
         protocol: 'https',
-        hostname: '**.akamaized.net', // Akamai based CDNs (N11 etc)
+        hostname: '**.akamaized.net',
       },
       {
         protocol: 'https',
         hostname: '**.ciceksepeti.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.trendyol.com',
       }
     ],
   },
